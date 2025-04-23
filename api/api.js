297765c445efcfaf9dd1a2b9fe7ed3da78fc5c2b
YuXiaosen0 <22373336@buaa.edu.cn>
2025-04-23 22:51:28 +0800

    [debug]编译文件bug

--- api/api.js
+++ api/api.js
@@ -1,78 +1,173 @@
-import http from "./http.js";
+import http from "./http";
 import {useUserStore} from '../stores/user.js'
 //首页信息
-export const getIndexList = () => {
-	return http('/test/hello','d','GET')
+// 获取队伍列表（通过主题 ID）
+/* export const getTeamList = ( themeId, pageNo = 1, pageSize = 4, isAsc = true ) => {
+  const bodyData = { pageNo, pageSize, isAsc }
+  return http('/team/list', {
+    params: { themeId },
+    body: bodyData
+  }, 'POST')
+} */
+export const getTeamList = ( themeId ) => {
+  return http(`/team/list?themeId=${themeId}`, {
+    "pageNo": 1,
+    "pageSize": 100,
+    "isAsc": true,
+    "sortBy": "name"
+}, 'POST')
 }
+
+
+// 获取主题列表
+export const getThemeList = () => {
+	return http(`/theme/list`, '', 'GET')
+}
+
+// 获取队伍详情
+export const getTeamDetails = (teamId) => {
+	return http(`/team/${teamId}`, '', 'GET')
+}
+
+// 获取公告（轮播图）列表
+export const getSwiperList = () => {
+	return http(`/message/announcement/list`, '', 'GET')
+}
+
+// 获取公告详情
+export const getAnnouncementDetails = (announcementId) => {
+	return http(`/message/announcement/${announcementId}`, '', 'GET')
+}
+
+//申请加入队伍
+export const applyToJoin = (teamId, joinReason) => {
+  return http(`/team/${teamId}/join/apply`, { "reason":joinReason }, 'POST')
+}
+
+// 获取申请列表
+export const getApplicationList = (teamId) => {
+	return http(`/team/${teamId}/join/list`, '', 'GET')
+}
+
+// 审批加入请求 0同意 1拒绝
+export const processApplication = (teamId, applicationId, action, comment) => {
+	return http(`/team/${teamId}/join/review/${applicationId}`, {
+		"action": action,
+		"comment": comment
+	  }, 'POST')
+}
+
+// 退出队伍
+export const leaveTeamApi = (teamId) => {
+	return http(`/team/${teamId}/leave`, '', 'POST')
+}
+
+// 判断身份
+export const judgeRole = (teamId) => {
+	return http(`/team/${teamId}/role`, '', 'GET')
+}
+
+// 踢出成员
+export const kickMember = (teamId, userId) => {
+	return http(`/team/${teamId}/kick/${userId}`, '', 'POST')
+}
+
+export const searchTeam = async (searchString) => {
+  return http(`/team/search?keyword=${searchString.keyward}`, { searchString }, 'GET')
+};
+
 // 登录
 export const login = (code) => {
-	return http('/user/wxLogin', { code }, 'POST')
+  return http('/user/wxLogin', { code }, 'POST')
 }
 // 获取用户信息
 export const getUserInfo = () => {
-	return http('/user/info','d','GET')
+  return http('/user/info','d','GET')
 }
 // 修改用户信息
 export const modifyUserInfo = (data) => {
-	return http('/user',data,'PUT')
+  return http('/user',data,'PUT')
 }
 // 发送验证码
 export const sendVerifyCode = (data) => {
-	return http('/verify',data,'POST')
+  return http('/verify',data,'POST')
 }
 // 进行北航身份验证
 export const yanzheng = (data) => {
-	return http('/user/verify',data,'POST')
+  return http('/user/verify',data,'POST')
 }
 export const searchList = async (params) => {
-  // 实现搜索逻辑
-  // 例如：
-  // const response = await request('/api/search', params);
-  // return response;
-  return null
+  return null
 }
 export const signClass = (id) => {
-	return http(`/course/sign?courseScheduleId=${id}`,'r','POST')
+  return http(`/course/sign?courseScheduleId=${id}`,'r','POST')
 }
 // 获取课程信息
 export const getCourseInfo = (data) => {
-	return http(`/course/list?date=${data}`,'d','GET')
+  return http(`/course/list?date=${data}`,'d','GET')
 }
 
 // 获取签到任务列表
 export const getSign = (status) => {
-	return http(`/course/task/list?status=${status}`,'d','GET')
+  return http(`/course/task/list?status=${status}`,'d','GET')
 }
 //添加签到任务
 export const addSign = (courseId) => {
-	return http(`/course/task/add`,{courseId},'POST')
+  return http(`/course/task/add`,{courseId},'POST')
 }
 //删除签到任务,传入任务id
 export const deleteSign = (Id) => {
-	return http(`/course/task/${Id}`,'r','DELETE')
+  return http(`/course/task/${Id}`,'r','DELETE')
 }
 //获得学生的签到记录
 export const getSignRecord = (data) => {
-	return http(`/course/log`,data,'POST')
+  return http(`/course/log`,data,'POST')
 }
 //listAll
 export const listAll = () => {
-	return http(`/interest/all`,'d','GET')
+  return http(`/interest/all`,'d','GET')
 }
 //获取用户的兴趣
 export const getMyInterest = () => {
-	return http(`/interest/my`,'d','GET')
+  return http(`/interest/my`,'d','GET')
 }
 //添加用户兴趣
 export const addMyInterest = (interestId) => {
-	return http(`/interest/add?interestId=${interestId}`,{},'POST')
+  return http(`/interest/add?interestId=${interestId}`,{},'POST')
 }
 //删除用户兴趣
 export const deleteMyInterest = (interestId) => {
-	return http(`/interest/${interestId}`,'d','DELETE')
+  return http(`/interest/${interestId}`,'d','DELETE')
 }
 // 博雅选课认证
 export const boyaAuthentication = () => {
-	return http(`/boya/authentication`,'d','POST')
+  return http(`/boya/authentication`,'d','POST')
 }
-
+//查询目前所有博雅课程
+export const getBoyaCourse = (data) => {
+  return http(`/boya/get-course`,data,'GET')
+}
+//直接选一门博雅课程
+export const xuanBoya = (course_id) => {
+  return http(`/boya/choose-course`,{course_id},'POST')
+}
+//查看所有已选的博雅课程
+export const lookupYiXuan = () => {
+  return http(`/boya/choose-course`,'d','GET')
+}
+//退选一门博雅课程
+export const deleteBoya = (select_id) => {
+  return http(`/boya/choose-course/${select_id}`,'d','DELETE')
+}
+//预约抢课
+export const appointBoya = (course_id) => {
+  return http(`/boya/appointment`,{course_id},'POST')
+}
+// 获取当前所有预约请求
+export const getAppointList = () => {
+  return http(`/boya/appointment`,'d','GET')
+}
+//撤销预约
+export const cancelAppoint = (appointment_id) => {
+  return http(`/boya/appointment/${appointment_id}`,'d','DELETE')
+}
---
+++ unpackage/dist/dev/mp-weixin/api/api.js
@@ -0,0 +1,120 @@
+"use strict";
+const api_http = require("./http.js");
+require("../stores/user.js");
+const getTeamList = (themeId) => {
+  return api_http.http(`/team/list?themeId=${themeId}`, {
+    "pageNo": 1,
+    "pageSize": 100,
+    "isAsc": true,
+    "sortBy": "name"
+  }, "POST");
+};
+const getThemeList = () => {
+  return api_http.http(`/theme/list`, "", "GET");
+};
+const getTeamDetails = (teamId) => {
+  return api_http.http(`/team/${teamId}`, "", "GET");
+};
+const getSwiperList = () => {
+  return api_http.http(`/message/announcement/list`, "", "GET");
+};
+const getAnnouncementDetails = (announcementId) => {
+  return api_http.http(`/message/announcement/${announcementId}`, "", "GET");
+};
+const applyToJoin = (teamId, joinReason) => {
+  return api_http.http(`/team/${teamId}/join/apply`, { "reason": joinReason }, "POST");
+};
+const getApplicationList = (teamId) => {
+  return api_http.http(`/team/${teamId}/join/list`, "", "GET");
+};
+const processApplication = (teamId, applicationId, action, comment) => {
+  return api_http.http(`/team/${teamId}/join/review/${applicationId}`, {
+    "action": action,
+    "comment": comment
+  }, "POST");
+};
+const leaveTeamApi = (teamId) => {
+  return api_http.http(`/team/${teamId}/leave`, "", "POST");
+};
+const judgeRole = (teamId) => {
+  return api_http.http(`/team/${teamId}/role`, "", "GET");
+};
+const kickMember = (teamId, userId) => {
+  return api_http.http(`/team/${teamId}/kick/${userId}`, "", "POST");
+};
+const searchTeam = async (searchString) => {
+  return api_http.http(`/team/search?keyword=${searchString.keyward}`, { searchString }, "GET");
+};
+const login = (code) => {
+  return api_http.http("/user/wxLogin", { code }, "POST");
+};
+const getUserInfo = () => {
+  return api_http.http("/user/info", "d", "GET");
+};
+const modifyUserInfo = (data) => {
+  return api_http.http("/user", data, "PUT");
+};
+const sendVerifyCode = (data) => {
+  return api_http.http("/verify", data, "POST");
+};
+const yanzheng = (data) => {
+  return api_http.http("/user/verify", data, "POST");
+};
+const signClass = (id) => {
+  return api_http.http(`/course/sign?courseScheduleId=${id}`, "r", "POST");
+};
+const getCourseInfo = (data) => {
+  return api_http.http(`/course/list?date=${data}`, "d", "GET");
+};
+const getSign = (status) => {
+  return api_http.http(`/course/task/list?status=${status}`, "d", "GET");
+};
+const addSign = (courseId) => {
+  return api_http.http(`/course/task/add`, { courseId }, "POST");
+};
+const deleteSign = (Id) => {
+  return api_http.http(`/course/task/${Id}`, "r", "DELETE");
+};
+const getSignRecord = (data) => {
+  return api_http.http(`/course/log`, data, "POST");
+};
+const listAll = () => {
+  return api_http.http(`/interest/all`, "d", "GET");
+};
+const getMyInterest = () => {
+  return api_http.http(`/interest/my`, "d", "GET");
+};
+const addMyInterest = (interestId) => {
+  return api_http.http(`/interest/add?interestId=${interestId}`, {}, "POST");
+};
+const deleteMyInterest = (interestId) => {
+  return api_http.http(`/interest/${interestId}`, "d", "DELETE");
+};
+exports.addMyInterest = addMyInterest;
+exports.addSign = addSign;
+exports.applyToJoin = applyToJoin;
+exports.deleteMyInterest = deleteMyInterest;
+exports.deleteSign = deleteSign;
+exports.getAnnouncementDetails = getAnnouncementDetails;
+exports.getApplicationList = getApplicationList;
+exports.getCourseInfo = getCourseInfo;
+exports.getMyInterest = getMyInterest;
+exports.getSign = getSign;
+exports.getSignRecord = getSignRecord;
+exports.getSwiperList = getSwiperList;
+exports.getTeamDetails = getTeamDetails;
+exports.getTeamList = getTeamList;
+exports.getThemeList = getThemeList;
+exports.getUserInfo = getUserInfo;
+exports.judgeRole = judgeRole;
+exports.kickMember = kickMember;
+exports.leaveTeamApi = leaveTeamApi;
+exports.listAll = listAll;
+exports.login = login;
+exports.modifyUserInfo = modifyUserInfo;
+exports.processApplication = processApplication;
+exports.searchTeam = searchTeam;
+exports.sendVerifyCode = sendVerifyCode;
+exports.signClass = signClass;
+exports.yanzheng = yanzheng;
+//# sourceMappingURL=../../.sourcemap/mp-weixin/api/api.js.map
